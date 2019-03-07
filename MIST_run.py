1c6e45d3bef17a52f3dd04f3ad15a74c836b1c9c
Ruben D'Sa <dsaxx005@umn.edu>
2019-03-06 19:41:54 -0600

    Fixed hinge overshoot
    *Changed joint control from VELOCITY to POSITION. Solves the overshoot when setting a high force. Currently behaves like the NomQ Gen5
    *Adjusted the link collisions

--- MIST_run.py
+++ MIST_run.py
@@ -21,29 +21,32 @@ robotId = p.loadURDF("MIST.urdf",robotStartPos, robotStartOrientation)
 #     print(p.getLinkInfo(robotId, linkIndex=i))
 
 # hingeVelocity = 500 # Multi-rotor
-hingeVelocity = 500 # Fixed-wing
+# hingeVelocity = 500 # Fixed-wing
+hingePosition = 1.57
 
+
+p.setJointMotorControl2(robotId,
+                    jointIndex=0,
+                    controlMode=p.POSITION_CONTROL,
+                    targetPosition=hingePosition,
+                    force=1000)
+p.setJointMotorControl2(robotId,
+                    jointIndex=1,
+                    controlMode=p.POSITION_CONTROL,
+                    targetPosition=hingePosition,
+                    force=1000)
+p.setJointMotorControl2(robotId,
+                    jointIndex=2,
+                    controlMode=p.POSITION_CONTROL,
+                    targetPosition=hingePosition,
+                    force=1000)
 
 for i in range (10000): #Time to run simulation
     p.stepSimulation()
-    time.sleep(1./100.)
+    time.sleep(1./1000.)
     p.applyExternalForce(robotId, 
                     0, 
                     [0,0, 100],
                     [0,.25,0],
                     1)
-    p.setJointMotorControl2(robotId,
-                        jointIndex=0,
-                        controlMode=p.VELOCITY_CONTROL,
-                        targetVelocity=hingeVelocity,
-                        force=50)
-    p.setJointMotorControl2(robotId,
-                        jointIndex=1,
-                        controlMode=p.VELOCITY_CONTROL,
-                        targetVelocity=hingeVelocity,
-                        force=50)
-    p.setJointMotorControl2(robotId,
-                        jointIndex=2,
-                        controlMode=p.VELOCITY_CONTROL,
-                        targetVelocity=hingeVelocity,
-                        force=50)
+
