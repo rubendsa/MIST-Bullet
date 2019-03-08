b303c154050cc928b4e039210b0a5568361271e8
Ruben D'Sa <dsaxx005@umn.edu>
2019-03-07 20:35:19 -0600

    Added helper functions
    Added applyAction() and getUAVState()

--- MIST_run.py
+++ MIST_run.py
@@ -135,6 +135,44 @@ def tailsitterAttitudeControl():
                     1)
 
 
+###################     Helper functions   #####################
+# Action Vector
+def applyAction(actionVector, robotId):
+    m0, m1, m2, m3, c0, c1, c2, c3, h0, h1, h2 = actionVector
+
+    # Thrust for each Motor
+    p.applyExternalForce(robotId, 0, [0,0, m0], [0,-.28,0], 1) #Apply m0 force[N] on link0, w.r.t. local frame
+    p.applyExternalForce(robotId, 1, [0,0, m1], [0,-.28,0], 1) #Apply m1 force[N] on link1, w.r.t. local frame
+    p.applyExternalForce(robotId, 2, [0,0, m2], [0,-.28,0], 1) #Apply m2 force[N] on link2, w.r.t. local frame
+    p.applyExternalForce(robotId, 3, [0,0, m3], [0,-.28,0], 1) #Apply m3 force[N] on link3, w.r.t. local frame
+
+    # Visual of propeller spinning (not critical)
+    p.setJointMotorControl2(robotId, propIds[0], p.VELOCITY_CONTROL, targetVelocity=m0*10, force=1000) 
+    p.setJointMotorControl2(robotId, propIds[1], p.VELOCITY_CONTROL, targetVelocity=m1*10, force=1000)
+    p.setJointMotorControl2(robotId, propIds[2], p.VELOCITY_CONTROL, targetVelocity=m2*10, force=1000)
+    p.setJointMotorControl2(robotId, propIds[3], p.VELOCITY_CONTROL, targetVelocity=m3*10, force=1000)
+    
+    # Control surface deflection [rads]
+    p.setJointMotorControl2(robotId, ctrlSurfIds[0], p.POSITION_CONTROL, targetPosition=c0, force=1000)
+    p.setJointMotorControl2(robotId, ctrlSurfIds[1], p.POSITION_CONTROL, targetPosition=c1, force=1000)
+    p.setJointMotorControl2(robotId, ctrlSurfIds[2], p.POSITION_CONTROL, targetPosition=c2, force=1000)
+    p.setJointMotorControl2(robotId, ctrlSurfIds[3], p.POSITION_CONTROL, targetPosition=c3, force=1000)
+    
+    # Hinge angle [rads]
+    p.setJointMotorControl2(robotId, hingeIds[0], p.POSITION_CONTROL, targetPosition=h0, force=1000)
+    p.setJointMotorControl2(robotId, hingeIds[1], p.POSITION_CONTROL, targetPosition=h1, force=1000)
+    p.setJointMotorControl2(robotId, hingeIds[2], p.POSITION_CONTROL, targetPosition=h2, force=1000)
+
+# State Vector
+def getUAVState(robotId):
+    a, b, c, d, e, f = p.getLinkState(robotId, 0)
+    position = e # x,y,z
+    orientation = f #Quaternion
+    return position, orientation 
+
+
+
+
 ###################     RUN SIMULATION     #####################
 
 # tailsitterAttitudeControl()
@@ -144,14 +182,12 @@ simTime = 10000
 simDelay = 1./1000.
 
 
-
 for i in range (simTime): #Time to run simulation
     p.stepSimulation()
     time.sleep(simDelay)
 
-    setHingePosition(1.57)
-    # cycleEverything(i, simTime)
-    
+    # applyAction([500, 500, 500, 500, .3, .1, -.1, -.3, .2, .2, .2], robotId) #Example applyAction
+    print(getUAVState(robotId))
     
 
 
